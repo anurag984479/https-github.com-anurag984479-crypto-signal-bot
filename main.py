da32ff592ad116c999e42d7abb0fc3ee7b8f23ea
anurag984479 <151211041+anurag984479@users.noreply.github.com>
2026-05-09 16:56:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -296,18 +296,19 @@ def get_trend(symbol_key):
     for tf_name, (period, interval) in tf_map.items():
 
     if symbol_key == "BTC/USD":
-    tf = "1d" if tf_name == "daily" else "240m"
+        tf = "1d" if tf_name == "daily" else "240m"
 
-    df = fetch_ccxt("coinbase", "BTC/USD", tf=tf, limit=300)
+        df = fetch_ccxt("coinbase", "BTC/USD", tf=tf, limit=300)
 
-    if df is None:
-        df = fetch_ccxt("kraken", "BTC/USD", tf=tf, limit=300)
-        else:
-            df = fetch_yf(
-                MARKETS[symbol_key]["yf"],
-                period=period,
-                interval=interval
-            )
+        if df is None:
+            df = fetch_ccxt("kraken", "BTC/USD", tf=tf, limit=300)
+
+    else:
+        df = fetch_yf(
+            MARKETS[symbol_key]["yf"],
+            period=period,
+            interval=interval
+        )
 
         if df is None or len(df) < 50:
             return "NEUTRAL"
